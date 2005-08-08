207c81d1c0a52ae3e05f3f7dca940e0977655a1d
exarkun <>
2005-08-08 22:07:05 +0000

    refactor existing userbase commands; add user enumeration and disabling

--- Axiom/axiom/plugins/userbasecmd.py
+++ Axiom/axiom/plugins/userbasecmd.py
@@ -1,15 +1,99 @@
 
+import sys, getpass
+
 from zope.interface import classProvides
 
 from twisted.python import usage
-
 from twisted import plugin
 
-from axiom import iaxiom, userbase
+from axiom import iaxiom, attributes, userbase
 
 from xmantissa.website import WebSite
 
-import getpass
+class AxiomaticSubCommandMixin:
+    store = property(lambda self: self.parent.store)
+
+    def decodeCommandLine(self, cmdline):
+        """Turn a byte string from the command line into a unicode string.
+        """
+        codec = sys.stdin.encoding or sys.getdefaultencoding()
+        return unicode(cmdline, codec)
+
+    def install(self):
+        for ls in self.store.query(userbase.LoginSystem):
+            raise usage.UsageError("UserBase already installed")
+        else:
+            ls = userbase.LoginSystem(store=self.store)
+            ls.install()
+            return ls
+
+class Install(usage.Options, AxiomaticSubCommandMixin):
+    def postOptions(self):
+        self.install()
+
+class Create(usage.Options, AxiomaticSubCommandMixin):
+    synopsis = "<username> <domain> [password]"
+
+    def parseArgs(self, username, domain, password=None):
+        self['username'] = self.decodeCommandLine(username)
+        self['domain'] = self.decodeCommandLine(domain)
+        self['password'] = password
+
+    def postOptions(self):
+        for ls in self.store.query(userbase.LoginSystem):
+            break
+        else:
+            ls = self.install()
+        
+        msg = 'Enter new AXIOM password: '
+        while not self['password']:
+            password = getpass.getpass(msg)
+            second = getpass.getpass('Repeat to verify: ')
+            if password == second:
+                self['password'] = password
+            else:
+                msg = 'Passwords do not match.  Enter new AXIOM password: '
+        
+        try:
+            acc = ls.addAccount(self['username'],
+                                self['domain'],
+                                self['password'])
+        except userbase.DuplicateUser:
+            raise usage.UsageError("An account by that name already exists.")
+
+
+class Disable(usage.Options, AxiomaticSubCommandMixin):
+    synopsis = "<username> <domain>"
+
+    def parseArgs(self, username, domain):
+        self['username'] = self.decodeCommandLine(username)
+        self['domain'] = self.decodeCommandLine(domain)
+
+    def postOptions(self):
+        for acc in self.store.query(userbase.LoginAccount,
+                                    attributes.AND(userbase.LoginAccount.username == self['username'],
+                                                   userbase.LoginAccount.domain == self['domain'])):
+            if acc.disabled:
+                raise usage.UsageError("That account is already disabled.")
+            else:
+                acc.disabled = True
+                break
+        else:
+            raise usage.UsageError("No account by that name exists.")
+
+
+class List(usage.Options, AxiomaticSubCommandMixin):
+    def postOptions(self):
+        acc = None
+        for acc in self.store.query(userbase.LoginAccount):
+            print acc.username + '@' + acc.domain,
+            if acc.disabled:
+                print '[DISABLED]'
+            else:
+                print
+        if acc is None:
+            print 'No accounts'
+        
 
 class UserBaseCommand(usage.Options):
     classProvides(plugin.IPlugin, iaxiom.IAxiomaticCommand)
@@ -17,29 +101,11 @@ class UserBaseCommand(usage.Options):
     name = 'userbase'
     description = 'Users.  Yay.'
 
-    optParameters = [
-        ('username', 'u', None, "The local-part of a user's ID"),
-        ('domain', 'd', None, "The domain-part of a user's ID"),
-        ('password', 'p', None, "The user's password")]
+    subCommands = [
+        ('install', None, Install, "Install UserBase on an Axiom database"),
+        ('create', None, Create, "Create a new user"),
+        ('disable', None, Disable, "Disable an existing user"),
+        ('list', None, List, "List users in an Axiom database"),
+        ]
 
-    def postOptions(self):
-        s = self.parent.getStore()
-        s.transact(self.doConfiguration, s)
-
-    def doConfiguration(self, s):
-        for ls in s.query(userbase.LoginSystem):
-            break
-        else:
-            ls = userbase.LoginSystem(store=s)
-            ls.install()
-        if self['username'] is not None:
-            if not self['domain']:
-                raise usage.UsageError(
-                    "If you specify a username, you must specify their domain.")
-            if not self['password']:
-                self['password'] = getpass.getpass('Enter new AXIOM password: ' %())
-            ls.addAccount(self['username'],
-                          self['domain'],
-                          self['password'])
-
-
+    store = property(lambda self: self.parent.getStore())
