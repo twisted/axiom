957cc65987bc30b2a691a442cd0d72926f63359a
glyph <>
2005-08-19 18:36:02 +0000

    It's the hip new thing that nobody can resist, RARA TAGS

---
+++ axiom/tags.py
@@ -0,0 +1,54 @@
+
+import itertools, operator
+
+from axiom.item import Item
+from axiom.attributes import text, reference, integer, AND, timestamp
+from axiom.extime import Time
+
+class Tag(Item):
+    typeName = 'tag'
+    schemaVersion = 1
+    name = text()
+
+    created = timestamp()
+
+    object = reference()
+    catalog = reference()
+    tagger = reference()
+
+class Catalog(Item):
+
+    typeName = 'tag_catalog'
+    schemaVersion = 1
+
+    tagCount = integer(default=0)
+
+    def tag(self, obj, tagName, tagger=None):
+        """
+        """
+        self.tagCount += 1
+        # check to see if that tag exists:
+        for t in self.store.query(Tag,
+                                  AND(Tag.object == obj,
+                                      Tag.name == tagName,
+                                      Tag.tagger == tagger)):
+            return
+        Tag(store=self.store, object=obj,
+            name=tagName, catalog=self,
+            created=Time(), tagger=tagger)
+
+    def tagsOf(self, obj):
+        """
+        Return an iterator of unicode strings (tag names).
+        """
+        return itertools.imap(operator.attrgetter('name'),
+                              self.store.query(Tag,
+                                               AND(Tag.object == obj,
+                                                   Tag.catalog == self)))
+
+    def objectsIn(self, tagName):
+        return itertools.imap(operator.attrgetter('object'),
+                              self.store.query(Tag,
+                                               AND(Tag.name == tagName,
+                                                   Tag.catalog == self)))
+
---
+++ axiom/test/test_tags.py
@@ -0,0 +1,36 @@
+
+from twisted.trial import unittest
+
+from axiom.store import Store
+from axiom.tags import Catalog
+
+from axiom.store import Item
+from axiom.attributes import text
+
+class Gizmo(Item):
+    typeName = 'test_gizmo'
+    schemaVersion = 1
+    name = text()
+
+
+
+class TagTestCase(unittest.TestCase):
+
+    def testTagging(self):
+        s = Store()
+        c = Catalog(store=s)
+        g1 = Gizmo(store=s, name=u'one')
+        g2 = Gizmo(store=s, name=u'two')
+
+        c.tag(g1, u'single')
+        c.tag(g1, u'multi')
+        c.tag(g2, u'multi')
+        c.tag(g1, u'multi')
+
+        self.assertEquals(list(c.tagsOf(g1)),
+                          [u'single', u'multi'])
+        self.assertEquals(list(c.tagsOf(g2)),
+                          [u'multi'])
+
+        self.assertEquals(list(c.objectsIn(u'single')), [g1])
+        self.assertEquals(list(c.objectsIn(u'multi')), [g1, g2])
