7359fe442d3cd4d149f2c75e509dddbd69f082b4
exarkun <>
2005-09-27 21:30:05 +0000

    SubScheduler and test

--- Axiom/axiom/scheduler.py
+++ Axiom/axiom/scheduler.py
@@ -18,46 +18,14 @@ class TimedEvent(Item):
     time = timestamp(indexed=True)
     runnable = reference()
 
-class Scheduler(Item, Service):
-
-    typeName = 'scheduler'
-    schemaVersion = 1
-
-    parent = inmemory()
-    running = inmemory()
-    name = inmemory()
-    timer = inmemory()
-
-    eventsRun = integer()
-    lastEventAt = timestamp()
-    nextEventAt = timestamp()
-
-    def activate(self):
-        self.timer = None
-        self.running = False
-
-    def __init__(self, **kw):
-        super(Scheduler, self).__init__(**kw)
-        self.eventsRun = 0
-        self.lastEventAt = None
-        self.nextEventAt = None
-
-    def installOn(self, other):
-        other.powerUp(self, IService)
-        other.powerUp(self, IScheduler)
-
+class SchedulerMixin:
     def now(self):
         # testing hook
         return Time()
 
-    def callLater(self, s, f, *a, **k):
-        s = max(s, 0.00000001)
-        return reactor.callLater(s, f, *a, **k)
-
     def tick(self):
         now = self.now()
         any = 0
-        self.timer = None
         self.nextEventAt = None
         before = self.eventsRun
         for event in self.store.query(TimedEvent,
@@ -77,21 +45,44 @@ class Scheduler(Item, Service):
         x = list(
             self.store.query(TimedEvent, sort=TimedEvent.time.ascending, limit=1))
         if x:
-            if self.timer is not None:
-                self.timer.cancel()
-            x = x[0]
-            self.timer = self.callLater(
-                x.time.asPOSIXTimestamp() - now.asPOSIXTimestamp(),
-                self.store.transact, self.tick)
-            self.nextEventAt = x.time
-        log.msg("%d events run" % (self.eventsRun - before,))
-
+            self._reschedule(x[0].time, now)
+        log.msg("%r ran %d events" % (self, self.eventsRun - before,))
 
     def schedule(self, runnable, when):
         TimedEvent(store=self.store, time=when, runnable=runnable)
-        if self.running:
-            if self.timer is None:
-                self.timer = self.callLater(0.00001, self.store.transact, self.tick)
+        self._reschedule(when, Time())
+
+
+class Scheduler(Item, Service, SchedulerMixin):
+
+    typeName = 'axiom_scheduler'
+    schemaVersion = 1
+
+    parent = inmemory()
+    running = inmemory()
+    name = inmemory()
+    timer = inmemory()
+
+    eventsRun = integer()
+    lastEventAt = timestamp()
+    nextEventAt = timestamp()
+
+    def __init__(self, **kw):
+        super(Scheduler, self).__init__(**kw)
+        self.eventsRun = 0
+        self.lastEventAt = None
+        self.nextEventAt = None
+
+    def __repr__(self):
+        return '<Scheduler>'
+
+    def activate(self):
+        self.timer = None
+        self.running = False
+
+    def installOn(self, other):
+        other.powerUp(self, IService)
+        other.powerUp(self, IScheduler)
 
     def startService(self):
         super(Scheduler, self).startService()
@@ -99,6 +90,53 @@ class Scheduler(Item, Service):
 
     def stopService(self):
         super(Scheduler, self).stopService()
-        if hasattr(self, 'timer'):
+        if self.timer is not None:
             self.timer.cancel()
             self.timer = None
+
+    def tick(self):
+        self.timer = None
+        return super(Scheduler, self).tick()
+
+    def callLater(self, s, f, *a, **k):
+        s = max(s, 0.00000001)
+        return reactor.callLater(s, f, *a, **k)
+
+    def _reschedule(self, when, now):
+        if not self.running:
+            return
+        if self.timer is not None:
+            if self.timer.getTime() < when.asPOSIXTimestamp():
+                return
+            self.timer.cancel()
+        self.timer = self.callLater(when.asPOSIXTimestamp() - now.asPOSIXTimestamp(),
+                                    self.store.transact, self.tick)
+        self.nextEventAt = when
+
+class _SubSchedulerParentHook(Item):
+    schemaVersion = 1
+    typeName = 'axiom_subscheduler_parent_hook'
+
+    loginAccount = reference()
+
+    def run(self):
+        IScheduler(self.loginAccount).tick()
+
+class SubScheduler(Item, SchedulerMixin):
+    schemaVersion = 1
+    typeName = 'axiom_subscheduler'
+
+    eventsRun = integer(default=0)
+    lastEventAt = timestamp()
+    nextEventAt = timestamp()
+
+    def __repr__(self):
+        return '<SubScheduler for %r>' % (self.store,)
+
+    def installOn(self, other):
+        other.powerUp(self, IScheduler)
+
+    def _reschedule(self, when, now):
+        loginAccount = self.store.parent.getItemByID(self.store.idInParent)
+        hook = self.store.parent.findOrCreate(_SubSchedulerParentHook, loginAccount = loginAccount)
+        IScheduler(self.store.parent).schedule(hook, when)
