c6818dcf4613c5901bc7bc763ac1e2a4d5e9b32d
glyph <>
2005-10-17 15:56:17 +0000

    More docstrings.

--- Axiom/axiom/userbase.py
+++ Axiom/axiom/userbase.py
@@ -40,6 +40,21 @@ class Preauthenticated(object):
 
 
 class LoginAccount(Item):
+    """
+    I am an entry in a LoginBase.
+
+    @ivar username: A local-part of my user's identifier.
+    @ivar domain: The domain part of my user's identifier. [XXX See TODO below]
+
+    @ivar avatars: An Item which is adaptable to various cred client
+    interfaces.  Plural because it represents a collection of potentially
+    disparate implementors, such as an IResource for web access and an IContact
+    for SIP access.
+
+    @ivar disabled: This account has been disabled.  It is still
+    database-resident but the user should not be allowed to log in.
+
+    """
     schemaVersion = 1
     typeName = 'login'
 
@@ -55,6 +70,9 @@ class LoginAccount(Item):
     disabled = integer()
 
     def __conform__(self, interface):
+        """
+        For convenience, forward adaptation to my 'avatars' attribute.
+        """
         ifa = interface(self.avatars, None)
         return ifa
 
@@ -63,6 +81,10 @@ class SubStoreLoginMixin:
         return SubStore(self.store, ('account', domain, username))
 
 class LoginBase:
+    """
+    I am a database powerup which provides an interface to a collection of
+    username/password pairs mapped to user application objects.
+    """
     implements(IRealm, ICredentialsChecker)
 
     credentialInterfaces = (IUsernamePassword, IUsernameHashedPassword, IPreauthCredentials)
@@ -82,6 +104,28 @@ class LoginBase:
             return account
 
     def addAccount(self, username, domain, password, avatars=None):
+        """
+        Create a user account, add it to this LoginBase, and return it.
+
+        @param username: the user's name.
+
+        @param domain: the domain part of the user's name [XXX TODO: this
+        really ought to say something about whether it's a Q2Q domain, a SIP
+        domain, an HTTP realm, or an email address domain - right now the
+        assumption is generally that it's an email address domain, but not
+        always]
+
+        @param password: A shared secret.
+
+        @param avatars: (Optional).  An object which, if passed, will be used
+        by cred as the target of all adaptations for this user.  By default, I
+        will create a SubStore, and plugins can be installed on that substore
+        using the powerUp method to provide implementations of cred client
+        interfaces.
+
+        @return: an instance of a LoginAccount, with all attributes filled out
+        as they are passed in, stored in my store.
+        """
         username = unicode(username)
         domain = unicode(domain)
         if self.accountByAddress(username, domain) is not None:
