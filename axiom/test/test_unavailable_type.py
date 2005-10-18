186f812c9509bc37a0faf0fb4edfd325fd997ec2
moe <>
2005-10-18 00:32:58 +0000

    add failing test case for unavailable type classes

---
+++ axiom/test/test_unavailable_type.py
@@ -0,0 +1,25 @@
+from twisted.trial.unittest import TestCase
+
+class UnavailableTypeTestCase(TestCase):
+    def testUnavailable(self):
+        from axiom import attributes, item, store
+
+        def makeItem():
+            class MyItem(item.Item):
+
+                typeName = 'test_deadtype_myitem'
+                schemaVersion = 1
+
+                hello = attributes.integer()
+
+            return MyItem
+
+        storedir = self.mktemp()
+
+        theStore = store.Store(storedir)
+        makeItem()(store=theStore)
+
+        item = reload(item)
+        store = reload(store)
+
+        store.Store(storedir)
