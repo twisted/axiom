6f49d0ec9baef2b365cadd24ffaedfe27e9921c0
glyph <>
2006-02-09 11:55:20 +0000

    Improve error handling and reliability of the scheduler.
    Fixes #622
    
    Author: glyph
    Reviewer: exarkun
    
    This merge creates a structured log of scheduled events which have died with
    an error, for easier debugging and re-queuing.  It also adds the pluggable
    error-handling that was necessary for exarkun's message-delivery-pipeline
    branch.
    
    The semantics of transactions and timed events are considerably better-defined
    now: each run() is invoked in its own transaction, the same transaction which
    queried for the timed event, the same transaction which will adjust the timed
    event.  Previously this was just all completely wrong and one broken event
    handler would screw up a non-deterministic number of unrelated events.
    
    It also adds a few unit tests, which is good, but we still need some much more
    comprehensive tests.

--- Axiom/axiom/scheduler.py
+++ Axiom/axiom/scheduler.py
@@ -3,14 +3,25 @@
 from twisted.internet import reactor
 
 from twisted.application.service import Service, IService
-from twisted.python import log
+from twisted.python import log, failure
 
 from epsilon.extime import Time
 from epsilon import descriptor
 
 from axiom.item import Item
-from axiom.attributes import AND, timestamp, reference, integer, inmemory
+from axiom.attributes import AND, timestamp, reference, integer, inmemory, bytes
 from axiom.iaxiom import IScheduler
+
+class TimedEventFailureLog(Item):
+    typeName = 'timed_event_failure_log'
+    schemaVersion = 1
+
+    desiredTime = timestamp()
+    actualTime = timestamp()
+
+    runnable = reference()
+    traceback = bytes()
+
 
 class TimedEvent(Item):
     typeName = 'timed_event'
@@ -19,35 +30,98 @@ class TimedEvent(Item):
     time = timestamp(indexed=True)
     runnable = reference()
 
+    def invokeRunnable(self):
+        """
+        Run my runnable, and reschedule or delete myself based on its result.
+        Must be run in a transaction.
+        """
+
+        newTime = self.runnable.run()
+        if newTime is None:
+            self.deleteFromStore()
+        else:
+            self.time = newTime
+
+    def handleError(self, now, failureObj):
+        """ An error occurred running my runnable.  Check my runnable for an
+        error-handling method called 'timedEventErrorHandler' that will take
+        the given failure as an argument, and execute that if available:
+        otherwise, create a TimedEventFailureLog with information about what
+        happened to this event.
+
+        Must be run in a transaction.
+        """
+        errorHandler = getattr(self.runnable, 'timedEventErrorHandler', None)
+        if errorHandler is not None:
+            errorHandler(self, failureObj)
+            self.deleteFromStore()
+        else:
+            self._defaultErrorHandler(now, failureObj)
+
+    def _defaultErrorHandler(self, now, failureObj):
+        tefl = TimedEventFailureLog(store=self.store,
+                                    desiredTime=self.time,
+                                    actualTime=now,
+                                    runnable=self.runnable,
+                                    traceback=failureObj.getTraceback())
+        self.deleteFromStore()
+
+class _WackyControlFlow(Exception):
+    def __init__(self, eventObject, failureObject):
+        Exception.__init__(self, "User code failed during timed event")
+        self.eventObject = eventObject
+        self.failureObject = failureObject
+
+MAX_WORK_PER_TICK = 10
+
 class SchedulerMixin:
     def now(self):
         # testing hook
         return Time()
 
+    def _oneTick(self, now):
+        theEvent = self._getNextEvent(now)
+        if theEvent is None:
+            return False
+        try:
+            theEvent.invokeRunnable()
+        except:
+            raise _WackyControlFlow(theEvent, failure.Failure())
+        self.lastEventAt = now
+        return True
+
+    def _getNextEvent(self, now):
+        # o/` gonna party like it's 1984 o/`
+        theEventL = list(self.store.query(TimedEvent,
+                                          TimedEvent.time < now,
+                                          sort=TimedEvent.time.ascending,
+                                          limit=1))
+        if theEventL:
+            return theEventL[0]
+
 
     def tick(self):
         now = self.now()
-        any = 0
         self.nextEventAt = None
         before = self.eventsRun
-        for event in self.store.query(TimedEvent,
-                                      TimedEvent.time < now,
-                                      sort=TimedEvent.time.ascending):
-            self.eventsRun += 1
-            newTime = event.runnable.run()
-            if newTime is not None:
-                event.time = newTime
-            else:
-                event.deleteFromStore()
-            any = 1
-        if any:
-            self.lastEventAt = now
-        x = list(
-            self.store.query(TimedEvent, sort=TimedEvent.time.ascending, limit=1))
+        workBeingDone = True
+        workUnitsPerformed = 0
+        errors = 0
+        while workBeingDone and workUnitsPerformed < MAX_WORK_PER_TICK:
+            workUnitsPerformed += 1
+            try:
+                workBeingDone = self.store.transact(self._oneTick, now)
+            except _WackyControlFlow, wcf:
+                self.store.transact(wcf.eventObject.handleError, now, wcf.failureObject)
+                log.err(wcf.failureObject)
+                errors += 1
+                workBeingDone = True
+        x = list(self.store.query(TimedEvent, sort=TimedEvent.time.ascending, limit=1))
         if x:
             self._transientSchedule(x[0].time, now)
-        log.msg("%r ran %d events" % (self, self.eventsRun - before,))
-
+        log.msg("The scheduler ran %(eventCount)s events%(errors)s." % dict(
+                eventCount=workUnitsPerformed,
+                errors=(errors and (" (with %d errors)" % (errors,))) or ''))
 
     def schedule(self, runnable, when):
         TimedEvent(store=self.store, time=when, runnable=runnable)
@@ -118,7 +192,7 @@ class Scheduler(Item, Service, SchedulerMixin):
 
     def startService(self):
         super(Scheduler, self).startService()
-        self.store.transact(self.tick)
+        self.tick()
 
     def stopService(self):
         super(Scheduler, self).stopService()
@@ -142,7 +216,7 @@ class Scheduler(Item, Service, SchedulerMixin):
                 return
             self.timer.cancel()
         self.timer = self.callLater(when.asPOSIXTimestamp() - now.asPOSIXTimestamp(),
-                                    self.store.transact, self.tick)
+                                    self.tick)
         self.nextEventAt = when
 
 
