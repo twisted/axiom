826727d7b8841ce4508bd2ded3d4ee3948afd33e
exarkun <>
2006-03-02 15:43:50 +0000

    Merge indexer-subprocess-655-3 (no, seriously)
    Author: washort, exarkun
    Reviewer: moe
    Fixes #655
    
    This adds a child process, managed by a service automatically associated
    on-disk Stores, started when the Store service is started, stopped when it is
    stopped, accessible by adapting a Store to axiom.iaxiom.IBatchService, which
    processes certain kinds of batch tasks.
    
    Process management code is factored out of Sine and into a generally usable set
    of classes provided by Axiom.
    
    The hype indexer is changed to declare its work as out-of-process.  Messages
    are passed between the two processes to coordinate searches, due to the
    constraints hype places on concurrent access to the index.
    
    Historical unit tests are cleaned up to use setUp and tearDown properly, so as
    not to start services but never stop them.  Additionally, the code is now
    essentially identical across all of the historical tests, making it extremely
    ripe for factoring out.
    
    The API for reliable listeners is reified as axiom.iaxiom.IReliableListener and
    expanded to support suspend and resume messages.
    
    The search interface defined by mantissa is now asynchronous, requiring
    Deferred returns from count and search.  ClickChronicle's xapian
    search-provider has been updated to comply with this.
    
    Several debugging aids useful during the course of development of this branch
    have been added.  epsilon.spewer is a prettier version of
    twisted.python.util.spewer.  JuiceBoxes have a prettier repr() string.

--- Axiom/axiom/batch.py
+++ Axiom/axiom/batch.py
@@ -7,11 +7,19 @@ of data over an extended period of time.
 
 import weakref, datetime
 
-from twisted.python import reflect, failure, log
+from zope.interface import implements
 
-from epsilon import extime
+from twisted.python import reflect, failure, log, util
+from twisted.application import service
+from twisted.internet import task, defer, reactor, error
 
-from axiom import item, attributes
+from epsilon import extime, process, cooperator
+
+from vertex import juice
+
+from axiom import iaxiom, item, attributes
+
+VERBOSE = True
 
 _processors = weakref.WeakValueDictionary()
 
@@ -68,14 +76,32 @@ class _ReliableListener(item.Item):
     work.
     """)
 
+    style = attributes.integer(doc="""
+    Either L{iaxiom.LOCAL} or L{iaxiom.REMOTE}. Indicates where the
+    batch processing should occur, in the main process or a
+    subprocess.
+    """)
+
+    def __repr__(self):
+        return '<ReliableListener %s %r #%r>' % ({iaxiom.REMOTE: 'remote',
+                                                  iaxiom.LOCAL: 'local'}[self.style],
+                                                 self.listener,
+                                                 self.storeID)
+
+
     def _forwardWork(self, workUnitType):
+        if VERBOSE:
+            log.msg("%r looking forward from %r" % (self, self.forwardMark,))
         return self.store.query(
             workUnitType,
             workUnitType.storeID > self.forwardMark,
             sort=workUnitType.storeID.ascending,
             limit=2)
 
+
     def _backwardWork(self, workUnitType):
+        if VERBOSE:
+            log.msg("%r looking backward from %r" % (self, self.backwardMark,))
         return self.store.query(
             workUnitType,
             workUnitType.storeID < self.backwardMark,
@@ -84,6 +110,8 @@ class _ReliableListener(item.Item):
 
 
     def _doOneWork(self, workUnit):
+        if VERBOSE:
+            log.msg("Processing a unit of work: %r" % (workUnit,))
         try:
             self.store.transact(self.listener.processItem, workUnit)
         except:
@@ -116,28 +144,38 @@ class _ReliableListener(item.Item):
             self._doOneWork(workUnit)
         if first:
             raise _NoWorkUnits()
+        if VERBOSE:
+            log.msg("%r.step() returning False" % (self,))
         return False
 
 
 
 class _BatchProcessorMixin:
-    def step(self):
+    def step(self, style=iaxiom.LOCAL):
         now = extime.Time()
         first = True
 
         for listener in self.store.query(_ReliableListener,
-                                         _ReliableListener.processor == self,
+                                         attributes.AND(_ReliableListener.processor == self,
+                                                        _ReliableListener.style == style),
                                          sort=_ReliableListener.lastRun.ascending):
             if not first:
+                if VERBOSE:
+                    log.msg("Found more work to do, returning True from %r.step()" % (self,))
                 return True
             listener.lastRun = now
             try:
                 if listener.step():
+                    if VERBOSE:
+                        log.msg("%r.step() reported more work to do, returning True from %r.step()" % (listener, self))
                     return True
             except _NoWorkUnits:
-                pass
+                if VERBOSE:
+                    log.msg("%r.step() reported no work units" % (listener,))
             else:
                 first = False
+        if VERBOSE:
+            log.msg("No listeners left with work, returning False from %r.step()" % (self,))
         return False
 
 
@@ -148,7 +186,7 @@ class _BatchProcessorMixin:
         return now + datetime.timedelta(milliseconds=self.idleInterval)
 
 
-    def addReliableListener(self, listener):
+    def addReliableListener(self, listener, style=iaxiom.LOCAL):
         """
         Add the given Item to the set which will be notified of Items
         available for processing.
@@ -180,7 +218,8 @@ class _BatchProcessorMixin:
                           processor=self,
                           listener=listener,
                           forwardMark=forwardMark,
-                          backwardMark=backwardMark)
+                          backwardMark=backwardMark,
+                          style=style)
 
 
     def removeReliableListener(self, listener):
@@ -231,12 +270,18 @@ def processor(forType):
     """
     MILLI = 1000
     if forType not in _processors:
+        def __init__(self, *a, **kw):
+            item.Item.__init__(self, *a, **kw)
+            self.store.powerUp(self, iaxiom.IBatchProcessor)
+
         attrs = {
             '__name__': 'Batch_' + forType.__name__,
 
             '__module__': forType.__module__,
 
-            '__repr__': lambda self: '<Batch %r %d>' % (reflect.qual(self.workUnitType), self.storeID),
+            '__init__': __init__,
+
+            '__repr__': lambda self: '<Batch of %s #%d>' % (reflect.qual(self.workUnitType), self.storeID),
 
             'workUnitType': forType,
 
@@ -250,3 +295,229 @@ def processor(forType):
             (item.Item, _BatchProcessorMixin),
             attrs)
     return _processors[forType]
+
+
+
+class SetStore(juice.Command):
+    """
+    Specify the location of the site store.
+    """
+    commandName = 'Set-Store'
+    arguments = [('storepath', juice.Path())]
+
+
+
+class SuspendProcessor(juice.Command):
+    """
+    Prevent a particular reliable listener from receiving any notifications
+    until a L{ResumeProcessor} command is sent or the batch process is
+    restarted.
+    """
+    commandName = 'Suspend-Processor'
+    arguments = [('storepath', juice.Path()),
+                 ('storeid', juice.Integer())]
+
+
+
+class ResumeProcessor(juice.Command):
+    """
+    Cause a particular reliable listener to begin receiving notifications
+    again.
+    """
+    commandName = 'Resume-Processor'
+    arguments = [('storepath', juice.Path()),
+                 ('storeid', juice.Integer())]
+
+
+
+class BatchProcessingControllerService(service.Service):
+    """
+    Controls starting, stopping, and passing messages to the system process in
+    charge of remote batch processing.
+    """
+
+    def __init__(self, store):
+        self.store = store
+        self.setName("Batch Processing Controller")
+
+
+    def startService(self):
+        service.Service.startService(self)
+        tacPath = util.sibpath(__file__, "batch.tac")
+        proto = BatchProcessingProtocol()
+        self.batchController = process.ProcessController(
+            "batch", proto, tacPath)
+        return self.batchController.getProcess().addCallback(self._addStore)
+
+
+    def _addStore(self, proto):
+        return SetStore(storepath=self.store.dbdir).do(proto)
+
+
+    def stopService(self):
+        service.Service.stopService(self)
+        d = self.batchController.stopProcess()
+        d.addErrback(lambda err: err.trap(error.ProcessDone))
+        return d
+
+
+    def suspend(self, storepath, storeID):
+        return self.batchController.getProcess().addCallback(
+            SuspendProcessor(storepath=storepath, storeid=storeID).do)
+
+
+    def resume(self, storepath, storeID):
+        return self.batchController.getProcess().addCallback(
+            ResumeProcessor(storepath=storepath, storeid=storeID).do)
+
+
+
+class _SubStoreBatchChannel(object):
+    """
+    SubStore adapter for passing messages to the batch processing system
+    process.
+
+    SubStores are adaptable to L{iaxiom.IBatchService} via this adapter.
+    """
+    implements(iaxiom.IBatchService)
+
+    def __init__(self, substore):
+        self.storepath = substore.dbdir
+        self.service = iaxiom.IBatchService(substore.parent)
+
+
+    def suspend(self, storeID):
+        return self.service.suspend(self.storepath, storeID)
+
+
+    def resume(self, storeID):
+        return self.service.resume(self.storepath, storeID)
+
+
+
+def storeBatchServiceSpecialCase(st, pups):
+    if st.parent is not None:
+        return _SubStoreBatchChannel(st)
+    return service.IService(st).getServiceNamed("Batch Processing Controller")
+
+
+
+class BatchProcessingProtocol(process.JuiceChild):
+    siteStore = None
+
+    def __init__(self, service=None):
+        juice.Juice.__init__(self, False)
+        self.storepaths = []
+        self.service = service
+        if service is not None:
+            self.service.cooperator = cooperator.Cooperator()
+
+
+    def command_SET_STORE(self, storepath):
+        from axiom import store
+
+        assert self.siteStore is None
+
+        self.siteStore = store.Store(storepath)
+        self.subStores = {}
+        self.pollCall = task.LoopingCall(self._pollSubStores)
+        self.pollCall.start(10.0)
+        return {}
+    command_SET_STORE.command = SetStore
+
+
+    def command_SUSPEND_PROCESSOR(self, storepath, storeid):
+        return self.subStores[storepath.path].suspend(storeid).addCallback(lambda ign: {})
+    command_SUSPEND_PROCESSOR.command = SuspendProcessor
+
+
+    def command_RESUME_PROCESSOR(self, storepath, storeid):
+        return self.subStores[storepath.path].resume(storeid).addCallback(lambda ign: {})
+    command_RESUME_PROCESSOR.command = ResumeProcessor
+
+
+    def _pollSubStores(self):
+        from axiom import store, substore
+        try:
+            paths = set([p.path for p in self.siteStore.query(substore.SubStore).getColumn("storepath")])
+        except:
+            # See #658
+            log.err()
+        else:
+            for removed in set(self.subStores) - paths:
+                self.subStores[removed].disownServiceParent()
+                del self.subStores[removed]
+                if VERBOSE:
+                    log.msg("Removed SubStore " + removed)
+            for added in paths - set(self.subStores):
+                self.subStores[added] = BatchProcessingService(store.Store(added), style=iaxiom.REMOTE)
+                self.subStores[added].setServiceParent(self.service)
+                if VERBOSE:
+                    log.msg("Added SubStore " + added)
+
+
+
+class BatchProcessingService(service.Service):
+    """
+    Steps over the L{iaxiom.IBatchProcessor} powerups for a single L{axiom.store.Store}.
+    """
+    def __init__(self, store, style=iaxiom.LOCAL):
+        self.store = store
+        self.style = style
+        self.suspended = []
+
+
+    def suspend(self, storeID):
+        item = self.store.getItemByID(storeID)
+        self.suspended.append(item)
+        return item.suspend()
+
+
+    def resume(self, storeID):
+        item = self.store.getItemByID(storeID)
+        self.suspended.remove(item)
+        return item.resume()
+
+
+    def deferLater(self, n):
+        d = defer.Deferred()
+        reactor.callLater(n, d.callback, None)
+        return d
+
+
+    def items(self):
+        return self.store.powerupsFor(iaxiom.IBatchProcessor)
+
+
+    def step(self):
+        while True:
+            try:
+                items = list(self.items())
+            except:
+                # See #658
+                log.err()
+                yield self.deferLater(1.0)
+                continue
+
+            if VERBOSE:
+                log.msg("Found %d processors for %s" % (len(items), self.store))
+            while items and self.running:
+                item = items.pop()
+                if item not in self.suspended:
+                    if VERBOSE:
+                        log.msg("Stepping processor %r" % (item,))
+                    try:
+                        item.step(style=self.style)
+                    except:
+                        # See #658
+                        log.err()
+                else:
+                    if VERBOSE:
+                        log.msg("Skipping suspended processor %r" % (item,))
+                yield None
+            yield self.deferLater(1.0)
+
+
+    def startService(self):
+        service.Service.startService(self)
+        self.parent.cooperator.coiterate(self.step())
