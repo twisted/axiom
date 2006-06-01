ae6928531eaef01104486b7d7697540c7dd45b85
exarkun <>
2006-06-01 15:53:37 +0000

    Merge quotient-indexes-1054
    Author: exarkun
    Reviewer: glyph
    Fixes #992 and #1054
    
    To Axiom' tag functionality, this adds a more efficient way to get a list of
    all the unique tags that have been applied in a Catalog.  It also adds a benchmark
    for tags.
    
    To the benchmarking system, this adds support for excluding startup and shutdown
    code from the main benchmark results.  The benchmark reporter now emits two sets
    of stats per run, one for the whole thing and one for the section between a start
    and a stop command.
    
    Quotient is updated to use the new functionality of Axiom for finding tag names.
    
    All existing benchmarks have been updated to use the new functionality of the
    benchmarking system as well.

--- axiom/benchmarks/benchmark_itemcreation.py
+++ axiom/benchmarks/benchmark_itemcreation.py
@@ -3,6 +3,8 @@
 Benchmark creation of a large number of simple Items in a transaction.
 """
 
+from epsilon.scripts import benchmark
+
 from axiom.store import Store
 from axiom.item import Item
 from axiom.attributes import integer, text
@@ -16,8 +18,11 @@ def main():
     def txn():
         for x in range(10000):
             AB(a=x, b=unicode(x), store=s)
+
+    benchmark.start()
     s.transact(txn)
-    s.close()
+    benchmark.stop()
+
 
 if __name__ == '__main__':
     main()
