ab85f4149eb4e7135d9fa6020beda2c1e53f3794
washort <>
2008-10-07 16:01:49 +0000

    support deferred completion of jobs

--- Axiom/axiom/test/test_worker.py
+++ Axiom/axiom/test/test_worker.py
@@ -4,6 +4,7 @@ Tests for L{axiom.worker}.
 """
 
 from twisted.trial import unittest
+from twisted.python import log
 from twisted.internet.defer import Deferred
 
 from axiom.item import Item
@@ -16,10 +17,10 @@ class TestJob(Item):
     """
     Example item for scheduling.
     """
-    timesRun = integer(default=0, doc="""
-                                      Number of times this item's run method was
-                                      called.
-                                      """)
+    timesRun = integer(default=0,
+                       doc="""
+                           Number of times this item's run method was called.
+                           """)
 
     callback = inmemory(doc="""
                             A callable to be run when this item is
@@ -27,7 +28,7 @@ class TestJob(Item):
                             """)
     def run(self):
         self.timesRun += 1
-        self.callback(self)
+        return self.callback(self)
 
 
 class WorkerTest(unittest.TestCase):
@@ -45,22 +46,54 @@ class WorkerTest(unittest.TestCase):
         self.assertEqual(w.nowServing, 0)
         self.assertEqual(w.nextTicket, 0)
         t = TestJob(store=s, callback=None)
-        w.enqueue(t)
+        q = w.enqueue(t)
         self.assertEqual(w.nextTicket, 1)
-        q = s.findUnique(QueuedEvent)
         self.assertEqual(q.runnable, t)
         self.assertEqual(q.ticket, 0)
 
 
     def test_runQueuedJob(self):
         """
-        Enqueueing an item results in its 'run' method being called.
+        Enqueueing an item results in its 'run' method being called after the
+        worker is started.
         """
         d = Deferred()
         s = Store()
         w = Worker(store=s)
         t = TestJob(store=s, callback=d.callback)
-        w.enqueue(t)
+        q = w.enqueue(t)
         self.assertEqual(t.timesRun, 0)
         w.start()
         return d
+
+    def test_transactionality(self):
+        """
+        Queued events are not removed until the Deferred returned
+        from their runnable's 'run' method fires.
+        """
+        d = Deferred()
+        s = Store()
+        w = Worker(store=s)
+        t = TestJob(store=s, callback=lambda _: d)
+        q = w.enqueue(t)
+        w._runOneItem()
+        self.assertIdentical(s.findUnique(QueuedEvent), q)
+        d.callback(True)
+        self.assertEqual(s.query(QueuedEvent).count(), 0)
+
+    def test_moreTransactionality(self):
+        """
+        Queued events are removed if the run fails.
+        """
+        d = Deferred()
+        s = Store()
+        w = Worker(store=s)
+        t = TestJob(store=s, callback=lambda _: d)
+        q = w.enqueue(t)
+        w._runOneItem()
+        self.assertIdentical(s.findUnique(QueuedEvent), q)
+        err = RuntimeError("Your event asplode")
+        d.errback(err)
+        loggedErrors = self.flushLoggedErrors(RuntimeError)
+        self.assertEqual(err, loggedErrors[0].value)
+        self.assertEqual(s.query(QueuedEvent).count(), 0)
