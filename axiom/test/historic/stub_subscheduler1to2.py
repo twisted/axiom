07b2dfe6cc3d93bd08d15a54c13d3ebdd74456fc
exarkun <>
2010-07-16 16:55:26 +0000

    Change the subscheduler 1 to 2 upgrader stub to generate a real substore; fix the test to deal with this

--- axiom/test/historic/stub_subscheduler1to2.py
+++ axiom/test/historic/stub_subscheduler1to2.py
@@ -7,12 +7,14 @@ version 2.
 
 from axiom.test.historic.stubloader import saveStub
 
+from axiom.substore import SubStore
 from axiom.scheduler import SubScheduler
 from axiom.dependency import installOn
 
 
 def createDatabase(store):
-    installOn(SubScheduler(store=store), store)
+    sub = SubStore.createNew(store, ["substore"]).open()
+    installOn(SubScheduler(store=sub), sub)
 
 
 if __name__ == '__main__':
