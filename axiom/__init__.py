5c05a0bd1ed0d12db798705448872b3255abce1e
Laurens Van Houtven <_@lvh.io>
2014-01-12 11:53:44 +0100

    Use setuptools for setup.py, specify dependencies, don't rely on Twisted for egg_info

--- axiom/__init__.py
+++ axiom/__init__.py
@@ -1,8 +1,5 @@
 # -*- test-case-name: axiom.test -*-
-from twisted.python import versions
 from axiom._version import __version__
+from epsilon import asTwistedVersion
 
-def _asTwistedVersion(packageName, versionString):
-    return versions.Version(packageName, *map(int, versionString.split(".")))
-
-version = _asTwistedVersion("axiom", __version__)
+version = asTwistedVersion("axiom", __version__)
