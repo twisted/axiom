1c9844ac7ed9c450ca4c0789477480b59ff79242
Laurens Van Houtven <_@lvh.io>
2014-01-15 11:48:55 +0100

    Add a local asTwistedVersion implementation to Axiom, so as to not add another setup-time dependency on Epsilon

--- Axiom/axiom/__init__.py
+++ Axiom/axiom/__init__.py
@@ -1,5 +1,8 @@
 # -*- test-case-name: axiom.test -*-
 from axiom._version import __version__
-from epsilon import asTwistedVersion
+from twisted.python import versions
+
+def asTwistedVersion(packageName, versionString):
+    return versions.Version(packageName, *map(int, versionString.split(".")))
 
 version = asTwistedVersion("axiom", __version__)
