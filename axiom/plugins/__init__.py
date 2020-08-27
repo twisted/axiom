6f61b427508ff17f376406872f3a2649ebf8d058
Tristan Seligmann <mithrandi@mithrandi.net>
2020-08-27 14:18:33 +0200

    Remove trivial uses of Epsilon.

--- axiom/plugins/__init__.py
+++ axiom/plugins/__init__.py
@@ -4,9 +4,6 @@
 Package for plugins for interfaces in Axiom.
 """
 
-from epsilon.hotfix import require
-require('twisted', 'plugin_package_paths')
-
 from twisted.plugin import pluginPackagePaths
 __path__.extend(pluginPackagePaths(__name__))
 __all__ = []
